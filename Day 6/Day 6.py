98df06b5eb5116357d47441f4dfe864d67b74492
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-17 21:10:45 -0600

    Day 6 line split working

--- Day 6/Day 6.py
+++ Day 6/Day 6.py
@@ -8,6 +8,18 @@ output_file = 'Day 6\\Lights.png'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
+class Data_Read:
+    def __init__ (self, line):
+        numbers = re.findall(r"[+-]?\d+(?:\.\d+)?",line)
+        self.x1 = int(numbers[0])
+        self.y1 = int(numbers[1])
+        self.x2 = int(numbers[2])
+        self.y2 = int(numbers[3])
+        # print vars(self)
+    def __str__ (self):
+        return str(vars(self))
+
+
 def image_grid(grid):
     off = (0, 0, 0)
     on = (226, 25, 224)
@@ -50,5 +62,11 @@ for y in range(0,1000):
     for x in range(0,1000):
         grid[y][x] = OFF
 
-image_grid(grid)
+if "turn on" in lines[0]:
+    start_x = Data_Read(lines[0]).x1
+    start_y = Data_Read(lines[0]).y1
+    end_x = Data_Read(lines[0]).x2
+    end_y = Data_Read(lines[0]).y2
+    on_lights(grid,start_x,start_y,end_x,end_y)
 
+image_grid(grid)
