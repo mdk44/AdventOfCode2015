fe9e4c153bae3c7770ac9c1054e3ac9fe03a14bc
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-25 15:57:29 -0600

    Day 13 complete!

--- Day 13/Day 13.py
+++ Day 13/Day 13.py
@@ -34,3 +34,25 @@ for item in list(perm):
         max_happy = happiness
 
 print("Part 1: " + str(max_happy)) # Correct!
+
+for person in people:
+    seating[person,'Matt'] = 0
+    seating['Matt', person] = 0
+people.append('Matt')
+
+perm = permutations(people)
+max_happy = 0
+for item in list(perm):
+    happiness = 0
+    happiness += seating[item[0],item[len(item)-1]]
+    happiness += seating[item[len(item)-1],item[0]]
+    for i in range(1,len(item)):
+        happiness += seating[item[i],item[i-1]]
+        happiness += seating[item[i-1],item[i]]
+    if happiness > max_happy:
+        max_happy = happiness
+
+print("Part 2: " + str(max_happy)) # Correct!
+
+
+
