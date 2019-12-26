136c3f9293ee6874bb84ce64a4cdca41620f4f37
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-12-25 23:16:34 -0600

    Day 14 Complete!

--- Day 14/Day 14.py
+++ Day 14/Day 14.py
@@ -3,30 +3,55 @@ input_file = 'Day 14\\Input.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-def calc_deer(speed, time, rest, seconds):
+def calc_dist(speed, time, rest, seconds):
     elapsed = 0
     dist = 0
-    while elapsed <= seconds:
-        elapsed += time
-        elapsed += rest
-        dist += (speed*time)
+    flag = 0
+    while flag == 0:
+        i = 1
+        while i <= time:
+            elapsed += 1
+            dist += speed
+            i += 1
+            if elapsed > seconds:
+                flag = 1
+                i += time
+        i = 1
+        while i <= rest:
+            elapsed += 1
+            i += 1
+            if elapsed > seconds:
+                flag = 1
+                i += rest
     return dist
-    
-
 
 deer = []
 speed = []
 time = []
 rest = []
+points = []
 for line in lines:
     deer.append(line.split(' ')[0])
     speed.append(int(line.split(' ')[3]))
     time.append(int(line.split(' ')[6]))
     rest.append(int(line.split(' ')[13]))
-    print(line)
+    points.append(0)
 
 results = []
 for i in range(0,len(deer)):
-    results.append(calc_deer(speed[i],time[i],rest[i],2503))
+    results.append(calc_dist(speed[i],time[i],rest[i],2503))
 
-print("Part 1: " + str(max(results))) # Correct, although I feel like this doesn't account for deer who are running when the clock retires
+print("Part 1: " + str(max(results))) # Correct
+
+top_dist = 0
+winner = 0
+for i in range(0,2503):
+    curr_dist = []
+    for j in range(0,len(deer)):
+        curr_dist.append(calc_dist(speed[j],time[j],rest[j],i))
+    curr_dist.append(i)
+    for j in range(0,len(deer)):
+        if curr_dist[j] == max(curr_dist):
+            points[j] += 1
+
+print("Part 2: " + str(max(points))) # Correct
