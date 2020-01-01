e8d29d2df9136501ed4c6c2bec6955136a8043b1
mdk44 <matt.karam@gmail.com>
2020-01-01 13:26:39 -0600

    Day 21 continued

--- Day 21/Day 21.py
+++ Day 21/Day 21.py
@@ -13,7 +13,7 @@ weapons[0] = [8, 4, 0, False]
 weapons[1] = [10, 5, 0, False]
 weapons[2] = [25, 6, 0, False]
 weapons[3] = [40, 7, 0, False]
-weapons[4] = [74, 8, 0, False]
+weapons[4] = [74, 8, 0, True]
 armor[0] = [13, 0, 1, False]
 armor[1] = [31, 0, 2, False]
 armor[2] = [53, 0, 3, False]
@@ -26,15 +26,29 @@ rings[3] = [20, 0, 1, False]
 rings[4] = [40, 0, 2, False]
 rings[5] = [80, 0, 3, False]
 
+def equip():
+    atk = my_dmg
+    arm = my_arm
+    for i in range(0, len(weapons)):
+        if weapons[i][3] == True:
+            atk += weapons[i][1]
+    for i in range(0, len(armor)):
+        if armor[i][3] == True:
+            arm += armor[i][2]
+    for i in range(0, len(rings)):
+        if rings[i][3] == True:
+            atk += rings[i][1]
+            arm += rings[i][2]
+    return atk, arm
+
 def combat(my_hp, boss_hp):
     winner = 0
-    my_atk = 0
-    boss_atk = 0
-    my_atk = max(1, my_dmg - boss_arm)
-    boss_atk = max(1, boss_dmg - my_arm)
-    print("My Damage....." + str(my_atk))
+    my_atk, my_arm = equip()
+    boss_atk = boss_dmg - my_arm
+    my_atk = my_atk - boss_arm
+    print("My Damage....." + str(max(my_atk,1)))
     print("My Armor......" + str(my_arm))
-    print("Boss Damage..." + str(boss_atk))
+    print("Boss Damage..." + str(max(boss_atk,1)))
     print("Boss Armor...." + str(boss_arm))
     while winner == 0:
         my_hp -= boss_atk
@@ -45,18 +59,6 @@ def combat(my_hp, boss_hp):
         if boss_hp <= 0:
             print("You win.  Crack open a fresh Baileys!  I'm Old Gregg!!")
             break
-    return True
-
-
-for i in range(0, len(weapons)):
-    if weapons[i][3] == True:
-        my_dmg += weapons[i][1]
-for i in range(0, len(armor)):
-    if armor[i][3] == True:
-        my_arm += armor[i][2]
-for i in range(0, len(rings)):
-    if rings[i][3] == True:
-        my_dmg += rings[i][1]
-        my_arm += rings[i][2]
+    return True    
 
 combat(my_hp, boss_hp)
