5e0a2d64fc5fc509df6fb3e7a75c08a3e354ebc5
mdk44 <matt.karam@gmail.com>
2020-01-01 09:02:07 -0600

    Day 19 complete!

--- Day 19/Day 19.py
+++ Day 19/Day 19.py
@@ -4,12 +4,13 @@ text_file = open(input_file)
 lines = text_file.read().split('\n')
 
 inp_string = 'CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl'
-# inp_string = 'HOH' # Test, Answer = 4
-# inp_string = 'HOHOHO' # Test, Answer = 7
+# inp_string = 'HOH' # Test
+# inp_string = 'HOHOHO' # Test
 
 solutions = []
 
 replacements = dict()
+repl_p2 = dict()
 for line in lines:
     source = line.split(' => ')[0]
     target = line.split(' => ')[1]
@@ -40,8 +41,21 @@ def find_molecules(inp, src):
                     solutions.append(outp)
     return True
 
+def part_2(inp):
+    num = 0
+    for i in range(0, len(inp)):
+        if inp[i].isupper():
+            num += 1
+    # The following numbers only appear on the right side, not the left
+    num -= inp.count('Rn')
+    num -= inp.count('Ar')
+    num -= (2*inp.count('Y')) # Related to the pattern in the input file and how removing Y also removes subsequent variables.  Had to google this.
+    num -= 1 # To get back to e
+    return num
+
+# Part 1
 for src in replacements:
     find_molecules(inp_string, src)
 
 print("Part 1: " + str(len(solutions))) # Correct!
-    
+print("Part 2: " + str(part_2(inp_string))) # Correct!
