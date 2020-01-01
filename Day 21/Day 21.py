59a844ffc243d2f9834bfa8ec677b679b87481bf
mdk44 <matt.karam@gmail.com>
2020-01-01 15:00:25 -0600

    Day 21 complete!

--- Day 21/Day 21.py
+++ Day 21/Day 21.py
@@ -43,28 +43,61 @@ def equip(w, a, r1, r2):
     if r2 != 0:
         gold += rings[r2][0] 
         atk += rings[r2][1]
-        arm += rings[r2][2]    
+        arm += rings[r2][2]
     return atk, arm, gold
+
+def min_equip():
+    min_gold = 999
+    for w in range(1, 6):
+        for a in range(0, 6):
+            for r1 in range(0, 7):
+                for r2 in range(0, 7):
+                    if r1 != r2:
+                        atk, arm, gold = equip(w, a, r1, r2)
+                        if atk + arm > 9 and gold < min_gold:
+                            min_gold = gold
+                            min_w = w
+                            min_a = a
+                            min_r1 = r1
+                            min_r2 = r2
+    return min_w, min_a, min_r1, min_r2
+
+def max_equip():
+    max_gold = 0
+    for w in range(1, 6):
+        for a in range(0, 6):
+            for r1 in range(0, 7):
+                for r2 in range(0, 7):
+                    if r1 != r2:
+                        atk, arm, gold = equip(w, a, r1, r2)
+                        if atk + arm < 10 and gold > max_gold:
+                            max_gold = gold
+                            max_w = w
+                            max_a = a
+                            max_r1 = r1
+                            max_r2 = r2
+    return max_w, max_a, max_r1, max_r2
 
 def combat(my_hp, boss_hp):
     winner = 0
-    my_atk, my_arm, gold = equip(1, 0, 0, 0)
+    # w, a, r1, r2 = min_equip() # Part 1
+    w, a, r1, r2 = max_equip() # Part 2
+    my_atk, my_arm, gold = equip(w, a, r1, r2)
     boss_atk = boss_dmg - my_arm
     my_atk = my_atk - boss_arm
-    print("My Damage....." + str(max(my_atk,1)))
-    print("Boss Damage..." + str(max(boss_atk,1)))
+    # print("My Damage....." + str(max(my_atk,1)))
+    # print("Boss Damage..." + str(max(boss_atk,1)))
     while winner == 0:
-        my_hp -= boss_atk
-        if my_hp <= 0:
-            print("Boss wins.  You seen his downstairs mixup.")
-            print("Gold spent: " + str(gold))
-            break
         boss_hp -= my_atk
         if boss_hp <= 0:
             print("You win.  Crack open a fresh Baileys!  I'm Old Gregg!!")
             print("Gold spent: " + str(gold))
             break
+        my_hp -= boss_atk
+        if my_hp <= 0:
+            print("Boss wins.  You seen his downstairs mixup.")
+            print("Gold spent: " + str(gold))
+            break
     return True    
 
-#Just manually added stuff until I won for part 1
-combat(my_hp, boss_hp)
+combat(my_hp, boss_hp) # Correct for Part 1 and Part 2!
