90575a5f89a242e000917a0ae49dac3fd6971a76
mdk44 <matt.karam@gmail.com>
2020-01-02 06:28:26 -0600

    Setting up Day 22

--- Day 22/Day 22.py
+++ Day 22/Day 22.py
@@ -2,3 +2,16 @@ boss_hp = 55
 boss_dmg = 8
 my_hp = 50
 my_mana = 500
+
+def magic_missile(mana):
+    if mana >= 53:
+        mana -= 53
+        dmg = 4
+    return dmg, mana
+
+def drain(mana, hp):
+    if mana >= 73:
+        mana -= 73
+        dmg = 2
+        hp += 2
+    return dmg, mana, hp
