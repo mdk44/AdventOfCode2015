23d3aa11eb0b4a139b3b8cbead9e90495dd3a77b
mdk44 <matt.karam@gmail.com>
2020-01-04 10:27:36 -0600

    More Day 22

--- Day 22/Day 22.py
+++ Day 22/Day 22.py
@@ -3,7 +3,6 @@ boss_dmg = 8
 my_hp = 50
 my_mana = 500
 my_arm = 0
-mana_spent = 0
 
 def magic_missile(mana):
     if mana >= 53:
@@ -22,21 +21,51 @@ def shield(mana, turn):
         mana -= 113
     if turn > 0 and turn <= 6:
         arm = 7
-    return mana, arm
+        shield = True
+    return mana, arm, shield
 
 def poison(mana, turn):
     if mana >= 173 and turn == 6:
         mana -= 173
     if turn > 0 and turn <= 6:
         dmg = 3
-    return mana, dmg
+        poison = True
+    return mana, dmg, poison
     
 def recharge(mana, turn):
     if mana >= 229 and turn == 5:
         mana -= 229
     if turn >= 0 and turn < 5:
         mana += 101
-    return mana
+        recharge = True
+    return mana, recharge
+
+def combat(my_hp, my_mana, my_arm, boss_hp, boss_dmg):
+    winner = 0
+    mana_spent = 0
+    while winner == 0:
+        # My turn
+        mana, dmg = magic_missile(my_mana)
+        boss_hp -= dmg
+        my_mana -= mana
+        mana_spent += mana
+        if boss_hp <= 0:
+            print("You have won!")
+            print("Mana spent: " + str(mana_spent))
+            break
+        else:
+            print("You have damaged the boss for " + str(dmg) + " and his hp is now " + str(boss_hp))
+        # Boss turn
+        my_hp += my_arm
+        my_hp -= boss_dmg
+        if my_hp <= 0:
+            print("You lost. :'( ")
+            print("Mana spent: " + str(mana_spent))
+            break
+        else:
+            print("The boss has damaged you for " + str(boss_dmg - my_arm) + " and your hp is now " + str(my_hp))
+
+combat(my_hp, my_mana, my_arm, boss_hp, boss_dmg)
 
 # Need to figure out how many spells I need to cast to win.  add mana to mana_spent (do not undo this with Shield - mana spent is always mana spent)
 # Effect spells can be started on the same turn they end
