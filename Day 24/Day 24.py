d8bca9bb15efcb7f33d51a0dbed12f4820a4ad13
mdk44 <matt.karam@gmail.com>
2020-01-05 11:29:04 -0600

    Day 24 complete!

--- Day 24/Day 24.py
+++ Day 24/Day 24.py
@@ -7,15 +7,17 @@ lines = text_file.read().split('\n')
 numbers = []
 for line in lines:
     numbers.append(int(line))
-# numbers = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+# numbers = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11] # Test input
 
 total_weight = 0
 for number in numbers:
     total_weight += number
-ideal_weight = total_weight / 3
+# ideal_weight = total_weight / 3 # Part 1
+ideal_weight = total_weight / 4 # Part 2
 
-min_quant = 13710311357 # The value when multiplying the 5 largest numbers together
-for x in permutations(numbers, 6):
+min_quant = 15000000000
+# for x in permutations(numbers, 6): # Part 1
+for x in permutations(numbers, 4): # Part 2
     quant = 1
     if sum(x) == ideal_weight:
         for i in x:
@@ -23,4 +25,4 @@ for x in permutations(numbers, 6):
         if quant < min_quant:
             min_quant = quant
 
-print("Part 1: " + str(min_quant)) # Correct!
+print("Answer: " + str(min_quant)) # Correct!
